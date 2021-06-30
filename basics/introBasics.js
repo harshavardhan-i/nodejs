62eea4124fdec604b238987250f98a0a18cbec02
Harshavardhan I <harshavardhan_i@dell.com>
2021-06-30 16:35:14 +0530

    node.js done

---
+++ basics/introBasics.js
@@ -0,0 +1,20 @@
+// Node.js is a backend technology.
+// Node allows us to create backend using JS.
+// It is scalable and really fast.
+
+// Node JS allows javascript to run outside of browsers.
+// With node JS we can intearch directly with the hardware of computer/servers.
+// Helps build apps like atom editor.
+
+// Below command helps check version of node.
+// node --version 
+
+// Run JS in cmd - node <fileName>.js
+
+// Node REPL - Read Evaluation Print Loop
+// Allows to execute code in byte size chunks
+// Lets you run JS code in terminal just like browser console
+// node : command to enter REPL
+// .exit : command to exit REPL, alternatively use Ctrl+C twice.
+
+// Node comes with pre-built Modules(bunch of libraries)
---
+++ basics/modulesRequireInstall.js
@@ -0,0 +1,22 @@
+// In order to use modules/libraries/packages we need to 'require' it.
+
+// fs - Node inbuilt module for file system.
+const fs = require("fs");
+// Copy file.
+fs.copyFileSync("introBasics.js", "tempIntro.js");
+
+// Modules can be Internal or external.
+// NPM - Node package manager, handles external packages.
+// NPM is bundeled with node, installing node will install NPM.
+// NPM packages are reusable code.
+// npm init: initializes and creates package.json
+// package.json - description of npm packages within project, dependencies etc.
+// fileName.json - File format 
+
+// External package:
+// Look for packages in npmjs.org, install using npm and read docs to use.
+var superHeroes  = require("superheroes");
+var superHeroName = superHeroes.random();
+console.log(superHeroName);
+let superVillains = require("superVillains");
+console.log(superVillains.random());
